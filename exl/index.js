5768e6b89ccac01935186d44a160228b7c6d393e
sayashraaj <sayashraaj@gmail.com>
2022-07-02 17:54:45 +0530

    try catch error handling

--- exl/index.js
+++ exl/index.js
@@ -118,99 +118,109 @@ app.use(express.urlencoded({ extended: false }));
 
 app.post('/', async(req,res)=>{
 
-    console.log(req.body);
+    try{
+        console.log(req.body);
 
-  // req.body contains- array of {name of place (id), coords[2]}
-  //function generates dist(nxn)- no floyd warshall needed
+      // req.body contains- array of {name of place (id), coords[2]}
+      //function generates dist(nxn)- no floyd warshall needed
 
-  let departure_searches = []
-  let dict = {}, rev_dict = {}, z_dict = {}
-  for(let i=0;i<req.body.length; i++){
+      let departure_searches = []
+      let dict = {}, rev_dict = {}, z_dict = {}
+      for(let i=0;i<req.body.length; i++){
 
-    dict[req.body[i].id] = i;
-    rev_dict[i] = req.body[i].id;
-    z_dict[i] = req.body[i].z;
+        dict[req.body[i].id] = i;
+        rev_dict[i] = req.body[i].id;
+        z_dict[i] = req.body[i].z;
 
-    let arrival_location_ids = []
-    for(let i=0;i<req.body.length;i++) arrival_location_ids.push(req.body[i].id);
+        let arrival_location_ids = []
+        for(let i=0;i<req.body.length;i++) arrival_location_ids.push(req.body[i].id);
 
-    departure_searches.push({
-      "id": req.body[i].id,
-      "departure_location_id": req.body[i].id,
-      "arrival_location_ids": arrival_location_ids,
-      "transportation": {
-          "type": "driving"
-      },
-      "departure_time": "2021-09-21T08:00:00Z",
-      "travel_time": 3600,
-      "properties": [
-        "travel_time",
-        "distance"
-      ]
-    })
-  }
+        departure_searches.push({
+          "id": req.body[i].id,
+          "departure_location_id": req.body[i].id,
+          "arrival_location_ids": arrival_location_ids,
+          "transportation": {
+              "type": "driving"
+          },
+          "departure_time": "2021-09-21T08:00:00Z",
+          "travel_time": 3600,
+          "properties": [
+            "travel_time",
+            "distance"
+          ]
+        })
+      }
 
-  const reqobject = {
-    "locations": req.body,
-    "departure_searches": departure_searches
-  }
+      const reqobject = {
+        "locations": req.body,
+        "departure_searches": departure_searches
+      }
 
-  let matrix_array = await axios({
-    method: 'post',
-    url: 'https://api.traveltimeapp.com/v4/time-filter',
-    headers: {
-      'X-Application-Id': '5543f7a8',
-      'X-Api-Key': '1bcfbbc477ebcf6ed375498554b2ed75'
-    },
-    data: reqobject
-  });
-  // let matrix_array = dummy_response;
+      let matrix_array = await axios({
+        method: 'post',
+        url: 'https://api.traveltimeapp.com/v4/time-filter',
+        headers: {
+          'X-Application-Id': '5543f7a8',
+          'X-Api-Key': '1bcfbbc477ebcf6ed375498554b2ed75'
+        },
+        data: reqobject
+      });
+      // let matrix_array = dummy_response;
 
-  const n = matrix_array.data.results.length
+      const n = matrix_array.data.results.length
 
-  let dist = new Array(n)
-  for(let i=0;i<matrix_array.data.results.length;i++) dist[i]=new Array(n);
-  for(let i=0;i<matrix_array.data.results.length;i++){
-    let u=dict[matrix_array.data.results[i].search_id];
-    for(let j=0;j<matrix_array.data.results[i].locations.length;j++){
-      let v=dict[matrix_array.data.results[i].locations[j].id];
-      if(dist[u] && dist[u][v]) continue;
-      dist[u][v]=matrix_array.data.results[i].locations[j].properties[0].travel_time;
-      dist[v][u]=matrix_array.data.results[i].locations[j].properties[0].travel_time;
+      let dist = new Array(n)
+      for(let i=0;i<matrix_array.data.results.length;i++) dist[i]=new Array(n);
+      for(let i=0;i<matrix_array.data.results.length;i++){
+        let u=dict[matrix_array.data.results[i].search_id];
+        for(let j=0;j<matrix_array.data.results[i].locations.length;j++){
+          let v=dict[matrix_array.data.results[i].locations[j].id];
+          if(dist[u] && dist[u][v]) continue;
+          dist[u][v]=matrix_array.data.results[i].locations[j].properties[0].travel_time;
+          dist[v][u]=matrix_array.data.results[i].locations[j].properties[0].travel_time;
+        }
+      }
+
+      let arr = []
+      for(let i=0;i<req.body.length;i++){
+        let temparr = [req.body[i].start, req.body[i].end, req.body[i].z];
+        arr.push(temparr);
+      }
+
+      let {ans, vec} = tsp_main1(dist, arr);
+
+      vec = reformat_vec(vec, dist, rev_dict, z_dict);
+      vec_copy = vec;
+      // generate_ics(vec, z_dict);
+
+      res.json({ans, vec});
     }
-  }
-
-  let arr = []
-  for(let i=0;i<req.body.length;i++){
-    let temparr = [req.body[i].start, req.body[i].end, req.body[i].z];
-    arr.push(temparr);
-  }
-
-  let {ans, vec} = tsp_main1(dist, arr);
-
-  vec = reformat_vec(vec, dist, rev_dict, z_dict);
-  vec_copy = vec;
-  // generate_ics(vec, z_dict);
-
-  res.json({ans, vec});
+    catch(err){
+        res.json({err})
+    }
 })
 
 app.get('/calendar', (req,res)=>{
 
-  let events_arr = []
-  for(let i=0;i<vec_copy.length;i++){
-    events_arr.push({
-      start: vec_copy[i].start,
-      end: vec_copy[i].end,
-      location: vec_copy[i].name
-    })
-  }
+    try{
+      let events_arr = []
+      for(let i=0;i<vec_copy.length;i++){
+        events_arr.push({
+          start: vec_copy[i].start,
+          end: vec_copy[i].end,
+          location: vec_copy[i].name
+        })
+      }
 
-  const cal = ical({
-    events: events_arr
-  });
+      const cal = ical({
+        events: events_arr
+      });
 
-  cal.serve(res);
+      cal.serve(res);
+    }
+    catch(err){
+        res.json({err})
+    }
 })
 
 app.listen(PORT, ()=>{
